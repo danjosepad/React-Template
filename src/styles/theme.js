6ee9332f6fb70db477076c1eeeed852cd3ec480a
daniel.padilha <danielpadilhapessoal@Outlook.com>
2020-06-09 17:58:22 -0300

    feat: color theme added

---
+++ src/styles/theme.js
@@ -0,0 +1,27 @@
+export const colors = {
+  white: "#FFFFFF",
+  platinum: "#E4E7E6",
+  ghostWhite: "#FBFBFF",
+  beige: "#EFEFD0",
+
+  // Gray 
+  gainsboro: "#DCDCDC",
+  lightGray: "#D3D3D3",
+  silver: "#C0C0C0",
+  darkGray: "#A9A9A9",
+  gray: "#808080",
+  dimGray: "#696969",
+  lightSlateGray: "#778899",
+  SlateGray: "#708090",
+  darkSlateGray: "#2F4F4F",
+  black: "#000000",
+
+  cyan: "#01BAEF",
+  usafaBlue: "#004E89",
+  sapphire: "#0B4F6C",
+  
+  orangeEngineering: "#B80C09",
+  orangeCrayola: "#FF6B35",
+
+  richBlack: "#040F16",
+}
